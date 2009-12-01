dc61b2eb8fbffb627fc1d5a74a03ed8f3e9cb4c1
Colin Shea <colin@evaryont.me>
2009-12-01 18:52:40 -0500

    More slicing & dicing of the configuration. Signed-off-by: Colin Shea <colin@evaryont.me>
    It's getting awfully small!

--- config/awesome/zenburn/theme.lua
+++ config/awesome/zenburn/theme.lua
@@ -8,7 +8,7 @@
 
 -- {{{ Main
 theme = {}
-theme.wallpaper_cmd = { "echo 'bg not set by awesome';" }
+theme.wallpaper_cmd = { "echo 'bg not set by awesome'" }
 -- }}}
 
 -- {{{ Styles
@@ -24,7 +24,7 @@ theme.bg_urgent = "#3F3F3F"
 -- }}}
 
 -- {{{ Borders
-theme.border_width  = "2"
+theme.border_width  = "0"
 theme.border_normal = "#3F3F3F"
 theme.border_focus  = "#6F6F6F"
 theme.border_marked = "#CC9393"
