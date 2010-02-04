186f526c510b19210407f44376b667b3ac022280
Colin Shea <colin@evaryont.me>
2010-02-03 20:44:21 -0500

    More white space in symlink.rb, extra exclusions
    Former-commit-id: b035a694499fb15dc8055d9c240ed142d586d3cc

--- symlink.rb
+++ symlink.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/ruby
- 
+
 # Makes symlinks for all the configuration files except for this script and any
 # file with README in it.
- 
+
 require 'fileutils'
-Dir["*"].reject {|file| file =~ /README/ or file == $0 }.each do |file|
-  unless File.exists? File.expand_path(File.join(ENV['HOME'], ".#{file}"))
-    FileUtils.ln_s(File.expand_path(File.join(File.dirname(file), file)), File.expand_path(File.join(ENV['HOME'], ".#{file}")), :verbose => true)
-  else
-    warn "`#{File.expand_path(File.join(ENV['HOME'], ".#{file}"))}' already exists, skipping"
-  end
+[Dir["*"], Dir["config/*"]].flatten.reject do |file|
+    file =~ /README/ or
+    file == $0 or
+    file =~ %r[^config/?$] or
+    file =~ /website/
+end.each do |file|
+    dotfile = File.expand_path(File.join(ENV['HOME'], ".#{file}"))
+
+     unless File.exists? dotfile
+         FileUtils.ln_s(File.expand_path(file), dotfile, :verbose => true)
+     else
+         warn "`#{dotfile}' already exists, skipping"
+     end
 end
 
