995fb266572089a66388a23954392717f8c4457a
Colin Shea <colin@evaryont.me>
2010-02-14 14:48:28 -0500

    Semi-fool proof way to detect pacman-color & powerpill binaries
    Former-commit-id: f5d3a7780a201d2baa6deeeb799e6c26d5e5ffef

--- config/zsh/pacman.zsh
+++ config/zsh/pacman.zsh
@@ -4,17 +4,13 @@
 PACMAN_OPTIONS=""
 POWERPILL_OPTIONS=""
 
-if [[ "$(echo =pacman-color)" != "=pacman-color" ]] ; then
-    pacman_bin==pacman-color
-else
-    pacman_bin==pacman
-fi
+pacman_bin="$(whence pacman-color || whence pacman)"
 
-if [[ "$(echo =powerpill)" != "=powerpill" ]] ; then
-    alias pacman="$(echo =powerpill) $POWERPILL_OPTIONS --pacman-bin $pacman_bin $PACMAN_OPTIONS"
-else
+if [[ -z "$(whence powerpill)" ]] ; then
     alias pacman="$pacman_bin $PACMAN_OPTIONS"
+else
+    alias pacman="$(whence powerpill) $POWERPILL_OPTIONS --pacman-bin $pacman_bin $PACMAN_OPTIONS"
 fi
 
-alias    pacmanr="s $(whence pacman) -Rcnsu"
-alias    spacman="s $(whence pacman) -S"
+alias pacmanr="s $(whence pacman) -Rcnsu"
+alias spacman="s $(whence pacman) -S"
