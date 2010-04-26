2770b81dddd4f1ac69be0519bd7d70065c8944c7
Colin Shea <colin@evaryont.me>
2010-04-25 23:06:45 -0400

    Fix indentation, since I messed up.
    Thanks again, aavogt
    
    
    Former-commit-id: 2029837f71b9fb257a5c7241987be8a03f5fd7f3

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -27,46 +27,46 @@ import XMonad.Prompt
 import XMonad.Prompt.AppendFile
 
 main = xmonad $ let conf = ewmh kde4Config {
-    -- replace ; ewmh
-    -- kde4Config {
+                      -- replace ; ewmh
+                      -- kde4Config {
 
-        modMask = mod4Mask -- use the Windows button as mod
-      , terminal = "urxvt.sh"
+                          modMask = mod4Mask -- use the Windows button as mod
+                        , terminal = "urxvt.sh"
 
-      , manageHook = manageHook kdeConfig <+> myManageHook
-      , logHook = myLogHook
+                        , manageHook = manageHook kdeConfig <+> myManageHook
+                        , logHook = myLogHook
 
-    }
-    `additionalKeysP`
-    [ ("M-<Escape>", kill)
-    , ("M-<Space>", sendMessage NextLayout)
-    , ("M-r", refresh)
-    , ("M-j", windows W.focusDown)
-    , ("M-k", windows W.focusUp)
-    , ("M-x", goToSelected gsConfig)
-    -- MPC keyboard control
-    , ("<XF86AudioPlay>", spawn "exec mpc toggle")
-    , ("<XF86AudioStop>", spawn "exec mpc stop")
-    , ("<XF86AudioPrev>", spawn "exec mpc prev")
-    , ("<XF86AudioNext>", spawn "exec mpc next")
-    -- My keyboard (a G15) also includes volume controls, but KDE already
-    -- manages some of them.
-    -- For reference, the keys are <XF86AudioMute> <XF86AudioRaiseVolume> <XF86AudioLowerVolume>
+                      }
+                      `additionalKeysP`
+                      [ ("M-<Escape>", kill)
+                      , ("M-<Space>", sendMessage NextLayout)
+                      , ("M-r", refresh)
+                      , ("M-j", windows W.focusDown)
+                      , ("M-k", windows W.focusUp)
+                      , ("M-x", goToSelected gsConfig)
+                      -- MPC keyboard control
+                      , ("<XF86AudioPlay>", spawn "exec mpc toggle")
+                      , ("<XF86AudioStop>", spawn "exec mpc stop")
+                      , ("<XF86AudioPrev>", spawn "exec mpc prev")
+                      , ("<XF86AudioNext>", spawn "exec mpc next")
+                      -- My keyboard (a G15) also includes volume controls, but KDE already
+                      -- manages some of them.
+                      -- For reference, the keys are <XF86AudioMute> <XF86AudioRaiseVolume> <XF86AudioLowerVolume>
 
-    -- TODO: This will be replaced by a bashrun (but using zsh!) clone
-    , ("M-g", appendFilePrompt defaultXPConfig "/home/colin/notes/notes.txt")
+                      -- TODO: This will be replaced by a bashrun (but using zsh!) clone
+                      , ("M-g", appendFilePrompt defaultXPConfig "/home/colin/notes/notes.txt")
 
-    -- mpc control via 'normal' keys
-    , ("M-a", submap . M.fromList $
-        [ ((0, xK_l),     spawn "mpc next")
-        , ((0, xK_h),     spawn "mpc prev")
-        , ((0, xK_z),     spawn "mpc random")
-        , ((0, xK_space), spawn "mpc toggle")
-        ])
+                      -- mpc control via 'normal' keys
+                      , ("M-a", submap . M.fromList $
+                          [ ((0, xK_l),     spawn "mpc next")
+                          , ((0, xK_h),     spawn "mpc prev")
+                          , ((0, xK_z),     spawn "mpc random")
+                          , ((0, xK_space), spawn "mpc toggle")
+                          ])
 
-    -- Spawn the configured terminal
-    , ("M-<Enter>", spawn $ terminal conf)
-    ]
+                      -- Spawn the configured terminal
+                      , ("M-<Enter>", spawn $ terminal conf)
+                      ] in conf
 
 myManageHook = composeAll (
 
