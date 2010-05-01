cd26da336daa53bafa1c2f515e9ad47d7bd9ed84
Colin Shea <colin@evaryont.me>
2010-05-01 17:07:10 -0400

    Comment out the old key bindings, to avoid any errors

--- xmonad/xmonad.rewrite.hs
+++ xmonad/xmonad.rewrite.hs
@@ -43,95 +43,74 @@ key_bindings = \c -> mkKeymap c $
              , ("M-<Return>", spawn $ XMonad.terminal conf)
              ]
 
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
-
-    -- launch gmrun
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
-
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
-
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
-
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
-    ++
-
-    --
-    -- mod-[1..9], Switch to workspace N
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
-
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+--    -- launch dmenu
+--    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+--
+--    --  Reset the layouts on the current workspace to default
+--    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+--
+--    -- Resize viewed windows to the correct size
+--    , ((modm,               xK_n     ), refresh)
+--
+--    -- Move focus to the master window
+--    , ((modm,               xK_m     ), windows W.focusMaster  )
+--
+--    -- Swap the focused window and the master window
+--    , ((modm,               xK_Return), windows W.swapMaster)
+--
+--    -- Swap the focused window with the next window
+--    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+--
+--    -- Swap the focused window with the previous window
+--    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+--
+--    -- Shrink the master area
+--    , ((modm,               xK_h     ), sendMessage Shrink)
+--
+--    -- Expand the master area
+--    , ((modm,               xK_l     ), sendMessage Expand)
+--
+--    -- Push window back into tiling
+--    , ((modm,               xK_t     ), withFocused $ windows . (if M.member W.floating then W.float else W.sink))
+--
+--    -- Increment the number of windows in the master area
+--    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+--
+--    -- Deincrement the number of windows in the master area
+--    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+--
+--    -- Toggle the status bar gap
+--    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+--    -- See also the statusBar function from Hooks.DynamicLog.
+--    --
+--    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+--
+--    -- Quit xmonad
+--    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+--
+--    -- Restart xmonad
+--    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+--    ]
+--    ++
+--
+--    --
+--    -- mod-[1..9], Switch to workspace N
+--    --
+--    -- mod-[1..9], Switch to workspace N
+--    -- mod-shift-[1..9], Move client to workspace N
+--    --
+--    [((m .|. modm, k), windows $ f i)
+--        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+--        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+--    ++
+--
+--    --
+--    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+--    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+--    --
+--    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+--        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+--        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
 ------------------------------------------------------------------------
