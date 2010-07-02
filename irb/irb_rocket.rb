917fc7dc044b96b4168ac10db71b7fb780a2e71b
Colin Shea <colin@evaryont.me>
2010-07-01 21:35:29 -0400

    Make irb awesome.
    Wirble, method_extensions, hirb, boson, irb_rocket, net-http-spy,
    awesome_print, what's not to love?
    
    Also! Embedded irb_rocket 'cause I can't find it's gem, but anyways I
    needed to modify it so it uses AP instead of wirble. Because AP is
    awesome-r than wirble at printing stuff, that's why.
    
    
    Former-commit-id: 64b4ccba4f6772ad68fea726dc83c3d352206e2b

---
+++ irb/irb_rocket.rb
@@ -0,0 +1,83 @@
+# By genki, (C) 2009
+# http://github.com/genki/irb_rocket
+require 'rubygems'
+require 'terminfo'
+require 'wirble'
+
+Wirble.init
+
+module IRB
+  class CaptureIO
+    def initialize
+      @sin, @sout = IO.pipe
+      @ein, @eout = IO.pipe
+      @out, @err = STDOUT.dup, STDERR.dup
+    end
+
+    def capture(&block)
+      STDOUT.reopen(@sout)
+      STDERR.reopen(@eout)
+      block.call
+    ensure
+      play
+      STDOUT.reopen(@out)
+      STDERR.reopen(@err)
+    end
+
+    def print(*args)
+      @out.print(*args)
+    end
+
+    def play
+      play_io(@ein) do |buf|
+        @err.print Wirble::Colorize::Color.escape(:red) + buf + "\e[m"
+      end
+      play_io(@sin) do |buf|
+        @out.print buf
+      end
+    end
+
+  private
+    def play_io(io, &block)
+      buf = ""
+      buf << io.read_nonblock(1024) while true
+    rescue Errno::EAGAIN
+      block[buf]
+    rescue Exception => e
+      @out.print Wirble::Colorize::Color.escape(:yellow) + e.inspect
+    end
+  end
+
+  class Irb
+    alias :original_signal_status :signal_status
+    def signal_status(name, *args, &block)
+      if name == :IN_EVAL
+        print sc
+        @last_line = eval('line', block.binding)
+        @io = CaptureIO.new
+        @io.capture do
+          original_signal_status(name, *args, &block)
+        end
+      else
+        original_signal_status(name, *args, &block)
+      end
+    end
+
+    def output_value
+      return ' ' if @io.nil?
+      last = @context.io.prompt + @last_line.split("\n").last
+      @io.print(rc + cuu1 + (cuf1*last.length) + " " +
+        Wirble::Colorize::Color.escape(:blue) + "#=>" + sgr0 +
+        " " + @context.last_value.ai + cud1)
+    end
+
+  private
+    def terminfo; @terminfo ||= TermInfo.new end
+    def cuu1; terminfo.tigetstr('cuu1') end
+    def cud1; terminfo.tigetstr('cud1') end
+    def cuf1; terminfo.tigetstr('cuf1') end
+    def sgr0; terminfo.tigetstr('sgr0') end
+    def sc; terminfo.tigetstr('sc') end
+    def rc; terminfo.tigetstr('rc') end
+  end
+end
--- irbrc
+++ irbrc
@@ -1,15 +1,30 @@
 begin
-    require 'irb/ext/save-history'
-    require 'irb/completion'
+    require 'irb/ext/save-history' # Save history across sessions to a file
+    require 'irb/completion' # Tab complete objects/methods
+
+    require 'method_extensions' # Awesome extensions to Method
+    require 'method_extensions/method/super' # Mods core classes!
+    require 'bond'; Bond.start
+    Bond.load_gems 'hirb'; require 'hirb' # Hirb has custom bond completion
+    require 'net-http-spy' # Spy on Net::HTTP requests
+    require 'wirble'
+    require File.join(ENV['HOME'], '.irb/irb_rocket')
 
     Thread.abort_on_exception = true
     ARGV.concat ["--readline", "--prompt-mode", "simple"]
 
-    IRB.conf[:SAVE_HISTORY] = 500
-    IRB.conf[:HISTORY_FILE] = File.expand_path File.join(%w[~ .data irb_history])
+    IRB.conf[:HISTORY_FILE] = File.expand_path File.join(ENV['XDG_DATA_HOME'], "irb_history") # Where to store the history
+    IRB.conf[:SAVE_HISTORY] = 500 # How many lines to save in the history
+    IRB.conf[:USE_READLINE] = true # Use readline
+    IRB.conf[:IGNORE_EOF] = true # Ignore Ctrl-D, require 'exit' (like I have in zsh)
 
-    require 'pp'
-    pp IRB.conf[:HISTORY_FILE]
+    # Set awesome_print as the default formatter
+    require "ap"
+#    IRB::Irb.class_eval do
+#      def output_value
+#        ap @context.last_value
+#      end
+#    end
 
     # http://ozmm.org/posts/time_in_irb.html
     def time(times = 1)
@@ -23,6 +38,18 @@ begin
     # thanks rtomayko
     def IRB.reload
         load __FILE__
+    end
+
+    def copy(str)
+        IO.popen('xclip -i', 'w') { |f| f << str.to_s }
+    end
+
+    def paste
+        `xclip -o`
+    end
+
+    def ep
+        eval(paste)
     end
 
 # Make some noise whenever there is an exception, of any kind.
