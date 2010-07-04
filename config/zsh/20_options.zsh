5b5348bfe48006b5deed40d6e33a41b20e26a0be
Colin Shea <colin@evaryont.me>
2010-07-03 23:18:54 -0400

    History & pushd related zsh options
    Former-commit-id: bc84b4ec8ea8b031940e451295b0edeb89bc7bc0

--- config/zsh/20_options.zsh
+++ config/zsh/20_options.zsh
@@ -1,10 +1,18 @@
 setopt   interactive_comments # Allow comments in interactive mode
-setopt   share_history        # Share the history, live, between instances
 setopt   append_history       # Append, don't overwrite, the history file
 setopt   ignore_eof           # Ignore ^D to exit zsh
 setopt   auto_cd              # Just type in the directory, no 'cd' needed!
 setopt   extended_glob        # Advanced shell globs
 unsetopt beep                 # I hate beeps
+setopt   hist_verify          # Expand history expansions instead of executing
+setopt   hist_ignore_dups     # Don't record duplicate (contiguous) commands
+setopt   hist_ignore_space    # Don't record commands that begin with a space
+setopt   hist_reduce_blanks   # Remove superfluous blanks before recording
+setopt   hist_no_store        # Don't store invocations of `history`
+setopt   extended_history     # Save time related information to history
+#setopt   auto_pushd           # cd pushes the directory to the stack
+setopt   pushd_ignore_dups    # ignore duplicate directories
+setopt   pushd_silent         # don't output the directory
 
 # Paste a URL? Now auto quoted, yay! :D
 autoload -U url-quote-magic
