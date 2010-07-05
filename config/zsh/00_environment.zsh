359d0e3894b0855172a557058d94e96f360118d3
Colin Shea <colin@evaryont.me>
2010-07-04 23:04:19 -0400

    Test for $XDG_*/zsh, not just the XDG dir itself
    Since we're using them anyways, better make sure they're there.
    
    
    Former-commit-id: 162b9550faf124906514d51539f361d5d4670921

--- config/zsh/00_environment.zsh
+++ config/zsh/00_environment.zsh
@@ -52,17 +52,17 @@ export TMOUT=3600
 
 # XDG-related stuff
 export XDG_CACHE_HOME="${HOME}/.cache/"
-if [ ! -d $XDG_CACHE_HOME ] ; then
+if [ ! -d $XDG_CACHE_HOME/zsh ] ; then
     mkdir -p $XDG_CACHE_HOME/zsh
 fi
 
 export XDG_CONFIG_HOME="${HOME}/.config/"
-if [ ! -d $XDG_CONFIG_HOME ] ; then
+if [ ! -d $XDG_CONFIG_HOME/zsh ] ; then
     mkdir -p $XDG_CONFIG_HOME/zsh
 fi
 
 export XDG_DATA_HOME="${HOME}/.data/"
-if [ ! -d $XDG_DATA_HOME ] ; then
+if [ ! -d $XDG_DATA_HOME/zsh ] ; then
     mkdir -p $XDG_DATA_HOME/zsh
 fi
 
