e3ed2acfd1619c83f7399103ea432148f382a13d
Colin Shea <colin@evaryont.me>
2010-09-05 21:54:30 -0700

    new function: pss, which calls `$editor $(clyde -Ss $1)`
    Former-commit-id: 5dcafd1c3cfca9fc256a5655c3411f125338c76d

--- config/zsh/70_pacman.zsh
+++ config/zsh/70_pacman.zsh
@@ -16,4 +16,10 @@ if [ -x /usr/bin/clyde ] ; then
     # Pacman completion for clyde
     compdef clyde=pacman
     compdef sup-clyde=pacman
+
+    # Short alias / function to search with clyde then pan through them with vim
+    # TODO: Set up vim as a global pager, then pipe this output to $PAGER
+    pss() {
+        clyde -Ss $1 &> /tmp/pss.$1.pacmans && $EDITOR /tmp/pss.$1.pacmans
+    }
 fi
