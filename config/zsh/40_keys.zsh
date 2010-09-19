dbf39db25a7132533256e8201b92f361cea5ca68
Colin Shea <colin@evaryont.me>
2010-09-19 00:56:38 -0700

    Magic key binds for my 'top!

---
+++ config/zsh/40_keys.zsh
@@ -0,0 +1,17 @@
+bindkey -v # Vim mode!
+
+# autoload -U zkdb ; zkdb
+file="${XDG_CONFIG_HOME:-"$HOME/.config"}/zsh/zkdb-$TERM-${DISPLAY:-$VENDOR-$OSTYPE}"
+if [ -e $file ] ; then
+    source $file
+    [[ -n ${key[Left]}      ]] && bindkey "${key[Left]}"      backward-char
+    [[ -n ${key[Right]}     ]] && bindkey "${key[Right]}"     forward-char
+    [[ -n ${key[Up]}        ]] && bindkey "${key[Up]}"        up-line-or-search
+    [[ -n ${key[Down]}      ]] && bindkey "${key[Down]}"      down-line-or-search
+    [[ -n ${key[Home]}      ]] && bindkey "${key[Home]}"      beginning-of-line
+    [[ -n ${key[End]}       ]] && bindkey "${key[End]}"       end-of-line
+    [[ -n ${key[Backspace]} ]] && bindkey "${key[Backspace]}" backward-delete-char
+    [[ -n ${key[Delete]}    ]] && bindkey "${key[Delete]}"    delete-char
+fi
+
+bindkey '^W' backward-kill-word-match
