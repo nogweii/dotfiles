195393a989b7a425fff3c95b992209d8a4a39a91
Colin Shea <colin@evaryont.me>
2011-01-28 20:05:49 -0700

    Pianobar config. CLI pandora w00t!

---
+++ config/pianobar/eventcmd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# create variables
+while read L; do
+	k="`echo "$L" | cut -d '=' -f 1`"
+	v="`echo "$L" | cut -d '=' -f 2`"
+	export "$k=$v"
+done < <(grep -e '^\(title\|artist\|album\|stationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
+
+case "$1" in
+	songstart)
+		notify-send "Pandora Radio" "Now playing: $title by $artist"
+		;;
+
+#	songfinish)
+#		# scrobble if 75% of song have been played, but only if the song hasn't
+#		# been banned
+#		if [ -n "$songDuration" ] &&
+#				[ $(echo "scale=4; ($songPlayed/$songDuration*100)>50" | bc) -eq 1 ] &&
+#				[ "$rating" -ne 2 ]; then
+#			# scrobbler-helper is part of the Audio::Scrobble package at cpan
+#			# "pia" is the last.fm client identifier of "pianobar", don't use
+#			# it for anything else, please
+#			scrobbler-helper -P pia -V 1.0 "$title" "$artist" "$album" "" "" "" "$((songDuration/1000))" &
+#		fi
+#		;;
+
+	*)
+		if [ "$pRet" -ne 1 ]; then
+			notify-send "Pandora Raido" "$1 failed: $pRetStr"
+		elif [ "$wRet" -ne 1 ]; then
+			notify-send "Pandora Raido" "$1 failed: Network error: $wRetStr"
+		fi
+		;;
+esac
+
