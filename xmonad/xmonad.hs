0a9e35b4db0c50d982d37f08202ac4af615a55f9
Colin Shea <colin@evaryont.me>
2011-05-25 18:14:01 -0700

    Moving Pandora related code to a separate file
    Also cleaned up old comments from the main xmonad.hs file
    
    
    Former-commit-id: 36a39f3f9dab37792a002b10b7a4408bbb33a5e2

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,90 +5,24 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 
--- for dbus (gnome-session hang)
-import System.Environment
-import System.Cmd
-import Control.Concurrent
-
 import XMonad.Util.EZConfig
 
--- I'm being silly & dangerous... fun!
---import XMonad.Actions.GridSelect
-import XMonad.Darcs.GridSelect
-
-import Data.Foldable
-import qualified Data.Map as M
--- Arch users: install haskell-xdg-basedir from AUR
-import System.Environment.XDG.BaseDir
-import System.IO
-
-pandoraNavigation :: TwoD a (Maybe a)
-pandoraNavigation = makeXEventhandler $ shadowWithKeymap navKeyMap navDefaultHandler
- where navKeyMap = M.fromList [
-          ((0,xK_Escape), cancel)
-         ,((0,xK_Return), select >> pandoraNavigation) -- default: play/pause
-         ,((0,xK_h)     , move (-1,0)   >> select >> pandoraNavigation) -- left: mark tired
-         ,((0,xK_l)     , move (1,0)    >> select >> pandoraNavigation) -- right: skip
-         ,((0,xK_j)     , move (0,1)    >> select >> pandoraNavigation) -- down: ban
-         ,((0,xK_k)     , move (0,-1)   >> select >> pandoraNavigation) -- up: love
-         ,((0,xK_space) , setPos (0,0)  >> select >> pandoraNavigation) -- center: play/pause
-         ]
-       -- The navigation handler ignores unknown key symbols
-       navDefaultHandler = const pandoraNavigation
-
--- A monochrome colorizer based on window class
-monoColorizer = colorRangeFromClassName
-                     black  -- lowest inactive bg
-                     black  -- highest inactive bg
-                     black  -- active bg
-                     white  -- inactive fg
-                     white  -- active fg
-  where black = minBound
-        white = maxBound
-
-pandoraGSConfig = defaultGSConfig {
-      gs_cellheight = 30
-    , gs_cellwidth  = 100
-    , gs_navigate   = pandoraNavigation
-}
-
-pianobarCmd :: String -> IO ()
-pianobarCmd x = do path <- getUserConfigFile "pianobar" "ctl"
-                   writeFile path x
+-- from ~/.xmonad/lib/
+import Music.Pandora
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
 key_bindings = [ ("M-x",             pandoraSelect) ]
---             , ()
---]
-    where
-        stringList = [("||", io $ (pianobarCmd "p")),
-                      ("X8", io $ (pianobarCmd "-")),
-                      (">>", io $ (pianobarCmd "n")),
-                      ("..", io $ (pianobarCmd "t")),
-                      ("<3", io $ (pianobarCmd "+"))]
-        pandoraSelect = runSelectedAction pandoraGSConfig stringList
---compiled_bindings = \c -> mkKeymap c $ key_bindings
 
--- main = xmonad gnomeConfig {
---        modMask     = mod4Mask,
---        terminal    = "urxvt"
--- }
 main = xmonad $ gnomeConfig {
        terminal           = "urxvt"
      , borderWidth        = 2
-     , normalBorderColor  = "black"
-     , focusedBorderColor = "orange"
      , focusFollowsMouse  = True
      , modMask            = mod4Mask
      , keys               = keys gnomeConfig
---   , mouseBindings      = myMouseBindings
---   , layoutHook         = myLayout
      , handleEventHook    = ewmhDesktopsEventHook
      , startupHook        = ewmhDesktopsStartup
---   , logHook            = myLogHook
---   , manageHook         = myManageHook
     } `additionalKeysP` key_bindings
 
 --- vim: set syn=haskell nospell:
