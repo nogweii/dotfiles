8b45c890ffdf74d24c0ea066f95d819271453550
Colin Shea <colin@gryphon.(none)>
2011-05-25 18:48:07 -0700

    Further reducing xmonad.hs to it's bare essentials.
    Former-commit-id: 366fb06025fe72b9b93a8ab30fed8f963586649e

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,10 +1,5 @@
 import XMonad
 import XMonad.Config.Gnome
-
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-
 import XMonad.Util.EZConfig
 
 -- from ~/.xmonad/lib/
@@ -13,16 +8,14 @@ import Music.Pandora
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-key_bindings = [ ("M-x",             pandoraSelect) ]
+key_bindings = [ ("M-a",             pandoraSelect)
+               , ("M-<Escape>",      kill)
+               ]
 
 main = xmonad $ gnomeConfig {
        terminal           = "urxvt"
-     , borderWidth        = 2
-     , focusFollowsMouse  = True
      , modMask            = mod4Mask
      , keys               = keys gnomeConfig
-     , handleEventHook    = ewmhDesktopsEventHook
-     , startupHook        = ewmhDesktopsStartup
     } `additionalKeysP` key_bindings
 
 --- vim: set syn=haskell nospell:
