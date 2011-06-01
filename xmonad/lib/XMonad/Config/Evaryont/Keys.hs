cab888f9c8566708b2496147afeda4a9137eb7c5
Colin Shea <colin@evaryont.me>
2011-05-31 20:43:37 -0700

    Added some new key bindings.
    Former-commit-id: 11a56a30f5956f5f15874e1d62522700e66990c3

--- xmonad/lib/XMonad/Config/Evaryont/Keys.hs
+++ xmonad/lib/XMonad/Config/Evaryont/Keys.hs
@@ -1,11 +1,14 @@
 module XMonad.Config.Evaryont.Keys (
-      key_bindings
+      key_bindings,
+      toggleStrutsKey
       ) where
 
 import XMonad
 import XMonad.Config.Evaryont.Utils
 import Music.Pandora
 import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
@@ -14,7 +17,8 @@ key_bindings = [ ("M-a",           pandoraSelect) -- control pianobar
                , ("M-<Escape>",    kill) -- close current window
                , ("M-S-<Escape>",  spawn "xkill") -- click to kill an application
                , ("M-q",           spawn "gnome-screensaver-command -l") -- lock the screen
-               , ("M-S-q",         broadcastMessage ReleaseResources >> restart "xmonad" True) -- restart XMonad
+            -- , ("M-S-q",         broadcastMessage ReleaseResources >> restart "xmonad" True) -- restart XMonad
+               , ("M-S-q",         restart "xmonad" True) -- restart XMonad
                , ("M-C-q",         spawn "gnome-session-quit") -- shutdown/hibernate/suspend dialog
                , ("M-l",           moveTo Next NonEmptyWS) -- go to next workspace
                , ("M-h",           moveTo Prev NonEmptyWS) -- go to previous workspace
@@ -23,4 +27,12 @@ key_bindings = [ ("M-a",           pandoraSelect) -- control pianobar
                , ("M-<Backspace>", toggleWS) -- toggle which workspace you're on
                , ("M-t",           toggleFloat) -- toggle wether a window is floating
                , ("M-e",           nextScreen) -- go to the next physical screen (Xinerama)
+               , ("M-p",           spawn "/home/colin/bin/dmenu-run") -- application launcher
+               , ("M-S-p",         spawn "gnome-panel-screenshot -i") -- take a screenshot
+               , ("M-x",           goToSelected defaultGSConfig) -- application launcher
                ]
+
+toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- vim: set nospell:
