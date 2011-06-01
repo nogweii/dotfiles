7f4d7dcac46a01b2e38454f0b08e8571c6ab614b
Colin Shea <colin@evaryont.me>
2011-05-31 20:35:47 -0700

    Revamp of sup-clyde, now sudo-pacman()
    Features:
        * Integrates with pacmatic
        * Ensures '-Ss' will be colorzied (either via clyde or pacsearch)
        * 'clyde' will still exist even if the binary doesn't. (Falling back
          to pacman.) Yay muscle memory! Now, trying to retrain myself to
          use pacman instead of clyde.
        * Executing clyde or pacman will provide the same level of
          functionality. Both in terms of AUR support, but in zsh
          completion.
    
    Former-commit-id: ba1d5996e5d9f1a4181ac153326e88cca3c213ee

--- config/zsh/70_pacman.zsh
+++ config/zsh/70_pacman.zsh
@@ -1,25 +1,44 @@
 ########################
 # Pacman
 
+# default, fall-back.
+pacman_program=/usr/bin/pacman
+
 if [ -x /usr/bin/clyde ] ; then
-
-    sup-clyde() {
-        case $1 in
-            (-Ss | -Si | -Sg | -Q* | -T | -*h* | --help)
-                /usr/bin/clyde "$@" ;;
-            (-S* | -R* | -U | *)
-                /usr/bin/sudo /usr/bin/clyde --needed "$@" || return $? ;;
-        esac
-    }
-    alias clyde="sup-clyde"
-
-    # Pacman completion for clyde
-    compdef clyde=pacman
-    compdef sup-clyde=pacman
-
-    # Short alias / function to search with clyde then pan through them with vim
-    # TODO: Set up vim as a global pager, then pipe this output to $PAGER
-    pss() {
-        clyde -Ss $1 &> /tmp/pss.$1.pacmans && $EDITOR /tmp/pss.$1.pacmans
-    }
+    pacman_program=clyde
 fi
+
+export pacman_program
+
+# Short alias / function to search with clyde then pan through them with vim
+# TODO: Set up vim as a global pager, then pipe this output to $PAGER
+pss() {
+    $pacman_program -Ss $1 &> /tmp/pss.$1.pacmans && $EDITOR /tmp/pss.$1.pacmans
+}
+
+pacman-sudo() {
+    binary=$pacman_program
+    case $1 in
+        (-Ss) # getting pretty colors regardless if clyde has been installed
+            if [ "$binary" != "clyde" -a -x /usr/bin/pacsearch ] ; then
+                shift ; # pop the `-Ss`
+                binary="pacsearch"
+            fi
+            $binary "$@"
+        ;;
+        (-Si | -Sg | -Q* | -T | -*h* | --help)
+            $binary "$@"
+        ;;
+        (-S* | -R* | -U | *)
+            # we're doing sudo, so this is when pacmatic behaves
+            binary="pacmatic"
+            /usr/bin/sudo pacman_program=$pacman_program \
+                $binary --needed "$@" || return $?
+        ;;
+    esac
+}
+
+compdef pacman-sudo=$pacman_program
+compdef pacman=$pacman_program
+alias pacman="pacman-sudo"
+alias clyde="pacman-sudo"
