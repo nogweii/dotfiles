9c4881125ca38a4c55bc928b5533fc9030dd56b8
Colin Shea <colin@evaryont.me>
2012-08-19 10:29:01 -0700

    Merge with changes I've made to my work station
    Former-commit-id: 5b9b846ecaa746a2474e43a81415f41013e73f27

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,49 +1,22 @@
 import XMonad
-import XMonad.Config.Kde
 import XMonad.Util.EZConfig
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.LayoutHints (layoutHintsToCenter)
-import XMonad.Layout.NoBorders (smartBorders)
+import DBus.Client.Simple
 import XMonad.Hooks.DynamicLog
-import XMonad.Util.Loggers
-import XMonad.Util.Font (Align (AlignCenter))
-import XMonad.Util.Run (spawnPipe, hPutStrLn)
-import XMonad.Util.Replace
+import qualified System.Taffybar.XMonadLog as TaffyLog
 
 -- from ~/.xmonad/lib/
 import Music.Pandora
 import XMonad.Config.Evaryont.Keys
+-- This does a lot of the real connecting
+import XMonad.Config.Evaryont.Settings
 
-manage_hook = composeAll [
-       isKDETrayWindow --> doIgnore
-     , isFullscreen --> doFullFloat
-     , resource =? "stalonetray" --> doIgnore
-     , resource =? "gxmessage" --> doCenterFloat
-     , resource =? "xmessage" --> doCenterFloat
-     , className =? "Plasma" --> doFloat
-     -- redirect a window to some workspace.
--- , className =? "Firefox"  --> doF (W.shift (myWorkspaces !! 1)))
-       ]
-     <+> manageDocks
-     <+> manageHook kde4Config
-     <+> transience'
-
-layout_hook = smartBorders $ layoutHintsToCenter $ avoidStruts $ layoutHook kde4Config
-
--- pretty much every piece of the config is in lib/XMonad/Config/Evaryont - go
--- check that out instead of this boring main function!
-settings = kde4Config {
-           terminal   = "urxvt"
-         , modMask    = mod4Mask
-         , manageHook = manage_hook <+> manageHook kde4Config
-         , layoutHook = layout_hook
-         } `additionalKeysP` key_bindings
+-- Apply my keys on top of the default set (any conflicts? Mine win.)
+final_settings = settings `additionalKeysP` key_bindings
 
 main :: IO ()
 main = do
---     replace
---     h <- spawnPipe "startkde"
-       xmonad settings
-
---- vim: set syn=haskell nospell:
+       client <- connectSession -- Connect XMonad to DBus
+       let pp = defaultPP
+       xmonad final_settings { -- And...GO!
+              logHook = TaffyLog.dbusLog client pp <+> logHook settings
+       }
