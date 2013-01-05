9825c09220fabd4c163cffd098c2c8eb90c38c0d
Colin Shea <colin@evaryont.me>
2013-01-04 23:10:21 -0700

    Focus follows mouse, true. Explicitly.
    Former-commit-id: c86412c82c3bf8e1a41863217030a1c74db0e1f0

--- xmonad/lib/XMonad/Config/Evaryont/Config.hs
+++ xmonad/lib/XMonad/Config/Evaryont/Config.hs
@@ -17,12 +17,13 @@ import XMonad.Config.Evaryont.Logger
 
 -- XMonad's reason d'etierre
 evaryontConfig dbus = ewmh defaultConfig {
-           terminal        = terminal_choice
-         , modMask         = mod4Mask
-         , manageHook      = management_hook
-         , logHook         = (appletLogger dbus) <+> log_hook
-         , startupHook     = startup_hook
-         , handleEventHook = handle_events <+> fullscreenEventHook
-         , layoutHook      = layout_hook
-         , workspaces      = iconspaces
+           terminal          = terminal_choice
+         , modMask           = mod4Mask
+         , manageHook        = management_hook
+         , logHook           = (appletLogger dbus) <+> log_hook
+         , startupHook       = startup_hook
+         , handleEventHook   = handle_events <+> fullscreenEventHook
+         , layoutHook        = layout_hook
+         , workspaces        = iconspaces
+         , focusFollowsMouse = true
          } `additionalKeysP` key_bindings
