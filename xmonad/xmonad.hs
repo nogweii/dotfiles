fef2b65379736c42a4a1589f541f3b65f077362c
Colin Shea <colin@evaryont.me>
2013-01-04 23:16:12 -0700

    Delete the statusbar import, and related code
    Former-commit-id: c6cfe22cae4ae1f272eda0bdded3f52139299721

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,19 +8,12 @@ import qualified DBus.Client as DBus
 -- from ~/.xmonad/lib/
 -- This does a lot of the real connecting
 import XMonad.Config.Evaryont.Config
--- Just need to pull the coulbe of static string entries to pass to spawnPipe
-import XMonad.Config.Evaryont.Statusbar (workspace_dzen_command)
 -- xmonad-screenshot, a neat-o tool that will take a screenshot of every
 -- workspace in one go.
 import XMonad.Util.WorkspaceScreenshot
 
-import XMonad.Util.Run (spawnPipe)
-
 main :: IO ()
 main = do
-       -- Launch the two status bars that build the complete DZen bar
-       --workspace_bar_spawn   <- spawnPipe workspace_dzen_command
-       --status_bar_spawn      <- spawnPipe status_bar
        dbus <- DBus.connectSession
        getWellKnownName dbus
        -- Start the GTK event handler, for xmonad-screenshot
