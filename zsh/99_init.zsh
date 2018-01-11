e2b7488d9f591f7cf78a4664426e3a8ad6a92388
Colin Shea <colin@evaryont.me>
2018-01-10 20:20:29 -0700

    fix keychain support
    also, use the $commands trick for fortune

--- zsh/99_init.zsh
+++ zsh/99_init.zsh
@@ -14,14 +14,13 @@ function zle-line-init {
 zle -N zle-line-init
 zle -N zle-keymap-select
 
-if [ -x /usr/bin/keychain ] ; then
-    keychain --quiet # Start gpg-agent & ssh-agent, but don't add any keys (yet)
-    [ -f $HOME/.keychain/${HOSTNAME}-sh ] && source $HOME/.keychain/${HOSTNAME}-sh # Load ssh-agent environment variables
-    [ -f $HOME/.keychain/${HOSTNAME}-sh-gpg ] && source $HOME/.keychain/${HOSTNAME}-sh-gpg # ditto, gpg-agent
+if [ -n "${commands[keychain]}" ]; then
+    # Use keychain to launch a shared SSH agent across terminals
+    eval $(keychain --eval --quiet --timeout 30 --noask --agents ssh --absolute --dir $XDG_CACHE_HOME/keychain --host localhost)
 fi
 
 # Run fortune only if it's installed and we aren't connected to the machine via ssh
-if [ -x "$(which fortune 2>&1)" -a -z "$SSH_CONNECTION" ] ; then
+if [ -n "${commands[fortune]}" -a -z "$SSH_CONNECTION" ] ; then
     fortune -s # "Short" apothegms only
 fi
 
