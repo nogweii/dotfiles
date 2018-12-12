3bb0c6830efe60896343ec15d1c6b18aad103311
Evaryont <colin@evaryont.me>
2018-12-11 21:37:52 -0700

    set up aliases for colorization with grc

---
+++ zsh/13_grc_colors.zsh
@@ -0,0 +1,22 @@
+# If I have installed grc on a system, alias a bunch of commands to use it:
+if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+  # Set alias for commands with known configuration with grc.
+  local cmd cmds="
+      arping blkid cc cvs df diff dig dnf docker du env fdisk findmnt free gcc
+      getfacl getsebool gmake hping id ifconfig ioping iostat ip iptables
+      ip6tables iwconfig journalctl last lastb lastlog ldap lsattr lsblk lsmod
+      lsof lspci make mount mtr netstat nmap nping ntpdate ping ping6 ps semanage
+      showmount stat sysctl systemctl tcpdump traceroute traceroute6 tune2fs
+      ulimit uptime vmstats w wdiff whois
+  "
+
+  for cmd in ${(z)cmds}; do
+    [ "$cmd" = ';' ] && continue
+    # check if the command actually exists on the system to avoid an alias to nothing
+    if (( $+commands[$cmd] )) ; then
+      alias $cmd="grc --colour=auto $(whence $cmd)"
+    fi
+  done
+
+  unset cmds cmd
+fi
