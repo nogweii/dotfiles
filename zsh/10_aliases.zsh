417692079d3b1da238b44656618164984a66ab23
Evaryont <colin@evaryont.me>
2019-06-17 08:53:04 -0700

    alias vi & vim to neovim

--- zsh/10_aliases.zsh
+++ zsh/10_aliases.zsh
@@ -137,3 +137,8 @@ alias npm8gb='node --max-old-space-size=8192 /usr/bin/npm'
 
 alias server-ports="sudo ss -lntup | sed 's/users:((\"\([^\"]*\)\",pid.*$/\1/' | tail -n+2 | awk '{ printf \"%-17s %s/%s\n\", \$7, \$5, \$1 }' | sort"
 alias vim-plug-update='nvim +PlugInstall +PlugUpdate +PlugUpgrade +3sleep +qa!'
+
+if [ -n "${commands[nvim]}" ]; then
+    alias vi=nvim
+    alias vim=nvim
+fi
