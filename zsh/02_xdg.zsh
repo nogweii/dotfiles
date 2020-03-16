e88a30e81532017d88952e20d523d479eda50667
Colin Shea <colin@evaryont.me>
2020-03-16 06:49:38 +0000

    Move programming language directory settings to zshenv

--- zsh/02_xdg.zsh
+++ zsh/02_xdg.zsh
@@ -3,8 +3,6 @@
 # items, much less now!
 
 export GEMRC="${DOTSDIR}/gemrc"
-export GEM_SPEC_CACHE="${HOME}/.local/cache/gem"
-export GEM_HOME="${HOME}/.local/ruby"
 export BUNDLE_CONFIG="${DOTSDIR}/config/bundle/config"
 export npm_config_prefix=$HOME/.local/node
 export npm_config_userconfig="${DOTSDIR}/config/npmrc"
@@ -22,9 +20,6 @@ export ANSIBLE_SSH_CONTROL_PATH_DIR="${XDG_CACHE_HOME}/ansible/cp"
 
 export GIMP2_DIRECTORY="${XDG_CONFIG_HOME}/gimp"
 
-# Move the user base directory to a subdirectory beneath .local, so the bin,
-# lib, etc directories don't pollute the top level directory
-export PYTHONUSERBASE=~/.local/pypi
 export PYTHON_EGG_CACHE="${XDG_CACHE_HOME}/python-eggs"
 
 export GOPATH=~/.local/go
@@ -38,11 +33,6 @@ export MYSQL_HISTFILE="${XDG_CACHE_HOME}/mysql_history"
 export WGETRC="${DOTSDIR}/config/wgetrc"
 export VAGRANT_HOME="${XDG_DATA_HOME}/vagrant"
 
-# Set `pip install` to install to the user directory rather than system-wide
-export PIP_USER=yes
-
 # Move the kerberos credentials cache out of /tmp
 export KRB5CCNAME="${XDG_DATA_HOME}/krb5cc"
 export KRB5_CONFIG="${XDG_CONFIG_HOME}/krb5_ipa.conf"
-
-export CARGO_HOME="${HOME}/.local/cargo"
