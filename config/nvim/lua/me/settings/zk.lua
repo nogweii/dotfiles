5f9d0a53d7e4b4aa59a26378ebc73aaa9e45a6ab
Nogweii <me@nogweii.net>
2021-06-22 18:34:49 -0700

    configure additional zk LSP commands

--- config/nvim/lua/me/settings/zk.lua
+++ config/nvim/lua/me/settings/zk.lua
@@ -20,8 +20,27 @@ configs.zk = {
   }
 }
 
+configs.zk.index = function()
+  vim.lsp.buf.execute_command({
+    command = "zk.index",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  })
+end
+
+configs.zk.new = function(...)
+  vim.lsp.buf_request(0, 'workspace/executeCommand',
+    {
+        command = "zk.new",
+        arguments = {
+            vim.api.nvim_buf_get_name(0),
+            ...
+        },
+    },
+    function(_, _, result)
+      if not (result and result.path) then return end
+      vim.cmd("edit " .. result.path)
+    end
+  )
+end
+
 lspconfig.zk.setup(my_lsp.make_config())
-
---[[ FileType
-    python    lua require'lspconfig'["pylsp"].manager.try_add() ]]
-
