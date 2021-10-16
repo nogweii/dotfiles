d7872ef8894ee0b1548aef0f4d3320e00b55bb80
Colin Shea <colin@evaryont.me>
2021-10-16 10:20:13 -0700

    don't need the opt variable any more, either

--- config/nvim/lua/me/options.lua
+++ config/nvim/lua/me/options.lua
@@ -1,42 +1,40 @@
 -- NeoVim editor options.
 -- Looking for configuration for plugins? Check settings/* and settings.lua
 
-local opt = vim.opt
+vim.opt.showmode = false -- Hide the mode indication in the command bar, since my status bar has it
 
-opt.showmode = false -- Hide the mode indication in the command bar, since my status bar has it
+vim.opt.wrap = false                     -- Don't wrap lines
+vim.opt.errorbells = false               -- Disable any error bells
+vim.opt.laststatus = 2               -- Always show the status bar
+vim.opt.hidden = true                     -- Allow changing buffers even with modifications
+vim.opt.spell = true                      -- Enable spell check
+vim.opt.spellcapcheck = ''             -- Disable capitalization spell check
+vim.opt.title = true                      -- Modify the terminal title
+vim.opt.hlsearch = true                   -- Highlight search results
+vim.opt.incsearch = true                  -- Jump to the first match in real-time
+vim.opt.ignorecase = true                 -- Case insensitive search, by default.
+vim.opt.smartcase = true                  -- Case-sensitive if there any capital letters
+vim.opt.modeline = true                   -- Let individual files specify settings
+vim.opt.background = "dark"            -- Use dark colors over lighter ones
+vim.opt.fileformat= "unix"            -- Prefer UNIX line endings
+vim.opt.history = 10000              -- Remember lots of history for :
+vim.opt.wildmenu = true                   -- Show completion matches in the status bar
+vim.opt.wildmode = "longest,full"      -- Expand the longest common match, then all
+vim.opt.backspace= "indent,eol,start" -- Smart backspace in insert mode
+vim.opt.sidescroll=1               -- Scroll horizontally 1 column at a time
+vim.opt.sidescrolloff=7            -- Always show this at least this many columns
+vim.opt.fileencoding = "utf-8"         -- Default to assuming files are encoded in UTF-8
+vim.opt.updatetime=100             -- Millisecs idle before calling the CursorHold
 
-opt.wrap = false                     -- Don't wrap lines
-opt.errorbells = false               -- Disable any error bells
-opt.laststatus = 2               -- Always show the status bar
-opt.hidden = true                     -- Allow changing buffers even with modifications
-opt.spell = true                      -- Enable spell check
-opt.spellcapcheck = ''             -- Disable capitalization spell check
-opt.title = true                      -- Modify the terminal title
-opt.hlsearch = true                   -- Highlight search results
-opt.incsearch = true                  -- Jump to the first match in real-time
-opt.ignorecase = true                 -- Case insensitive search, by default.
-opt.smartcase = true                  -- Case-sensitive if there any capital letters
-opt.modeline = true                   -- Let individual files specify settings
-opt.background = "dark"            -- Use dark colors over lighter ones
-opt.fileformat= "unix"            -- Prefer UNIX line endings
-opt.history = 10000              -- Remember lots of history for :
-opt.wildmenu = true                   -- Show completion matches in the status bar
-opt.wildmode = "longest,full"      -- Expand the longest common match, then all
-opt.backspace= "indent,eol,start" -- Smart backspace in insert mode
-opt.sidescroll=1               -- Scroll horizontally 1 column at a time
-opt.sidescrolloff=7            -- Always show this at least this many columns
-opt.fileencoding = "utf-8"         -- Default to assuming files are encoded in UTF-8
-opt.updatetime=100             -- Millisecs idle before calling the CursorHold
+vim.opt.complete = vim.opt.complete + 'kspell'         -- Scan spell checker's dictionary for completion as well
+vim.opt.completeopt = "noinsert,menuone,noselect,preview"
+vim.opt.virtualedit = "block"         -- Block movement can go beyond end-of-line
+vim.opt.modelines = 3                -- Search the top and bottom 3 lines for modelines
+vim.opt.number = true                     -- Show line numbers
+vim.opt.undofile = true                   -- Persist undo history across sessions
+vim.opt.termguicolors = true              -- Use guifg over ctermfg in true-color terminals
 
-opt.complete = opt.complete + 'kspell'         -- Scan spell checker's dictionary for completion as well
-opt.completeopt = "noinsert,menuone,noselect,preview"
-opt.virtualedit = "block"         -- Block movement can go beyond end-of-line
-opt.modelines = 3                -- Search the top and bottom 3 lines for modelines
-opt.number = true                     -- Show line numbers
-opt.undofile = true                   -- Persist undo history across sessions
-opt.termguicolors = true              -- Use guifg over ctermfg in true-color terminals
-
-opt.sessionoptions = opt.sessionoptions
+vim.opt.sessionoptions = vim.opt.sessionoptions
               - "blank"        -- Don't save empty windows in the session
               - "buffers"      -- Don't save hidden buffers into the session
               - "help"         -- Ignore the help buffer for sessions
@@ -45,20 +43,20 @@ opt.sessionoptions = opt.sessionoptions
               + "localoptions" -- Include buffer local overrides
               + "tabpages"     -- This session is for all tabs, not individual ones
 
-opt.formatoptions = opt.formatoptions
+vim.opt.formatoptions = vim.opt.formatoptions
                  + "r"         -- Add comment syntax to new lines in insert mode
                  + "o"         -- Automatically add comment syntax after o/O
 
-opt.shortmess = vim.o.shortmess
+vim.opt.shortmess = vim.o.shortmess
                 .. "F" -- Don't print a message when opening a file
                 .. "c" -- silence insert mode completion messages
 
-opt.foldlevel = 5              -- Only fold sections deeper than this level automatically
-opt.foldlevelstart = 5         -- Only fold sections deeper than this level automatically
+vim.opt.foldlevel = 5              -- Only fold sections deeper than this level automatically
+vim.opt.foldlevelstart = 5         -- Only fold sections deeper than this level automatically
 
 
 -- ignore a bunch of stuff in the wildmenu completion
-opt.wildignore = {
+vim.opt.wildignore = {
   ".DS_Store", ".git", ".svn", ".hg",
   "*.a", "*.o", "*.obj", "*.out",
   "*.so", "*.dll", "*.exe", "*.bin",
@@ -71,19 +69,19 @@ opt.wildignore = {
 
 
 -- Point the spell checker at my additional vocabulary words
-opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
 
 -- Calculate the offset required to the cursor vertically centered on-screen.
-opt.scrolloff = vim.o.lines - 2
+vim.opt.scrolloff = vim.o.lines - 2
 
-opt.backupdir = vim.fn.stdpath("data") .. '/backup'
+vim.opt.backupdir = vim.fn.stdpath("data") .. '/backup'
 if not vim.fn.isdirectory(vim.o.backupdir) then
   vim.fn.mkdir(vim.o.backupdir, "p")
 end
 
 -- Don't always show the sign columns, but if there are, make sure there's room
 -- for two. This matches the width of the mode indicator in the statusbar
-opt.signcolumn = 'auto:2'
+vim.opt.signcolumn = 'auto:2'
 
 -- Add angle brackets to the list of recognized characters in a pair
-opt.matchpairs = vim.bo.matchpairs .. ",<:>"
+vim.opt.matchpairs = vim.bo.matchpairs .. ",<:>"
