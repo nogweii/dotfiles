3ddbb63d84eb6a139ac5d55648ce9ff38a0b776c
Colin Shea <colin.shea@irl.com>
2022-03-21 11:42:37 -0700

    update dial config for v0.3 changes

--- config/nvim/lua/me/settings/dial_swaps.lua
+++ config/nvim/lua/me/settings/dial_swaps.lua
@@ -1,7 +1,7 @@
 -- Add additional cycle groups for monaqa's dial plugin to make it behave
 -- similarly to SwapIt, toggle.vim, and/or cycle.vim
 
-local dial = require("dial")
+local augend = require("dial.augend")
 
 -- NB: these enumerated lists are active in every buffer, they aren't scoped to
 -- a particular file type... this could be unwanted behavior!
@@ -23,10 +23,39 @@ local addtional_enums = {
   cardinals = {"north", "east", "south", "west"},
 }
 
+local built_in_augends = {
+  augend.semver.alias.semver,
+  augend.integer.alias.decimal_int,
+  augend.integer.alias.hex,
+  augend.integer.alias.octal,
+  augend.integer.alias.binary,
+  augend.date.alias["%m/%d"],
+  augend.date.alias["%Y/%m/%d"],
+  augend.date.alias["%Y-%m-%d"],
+  augend.date.alias["%H:%M:%S"],
+  augend.date.alias["%H:%M"],
+  augend.constant.alias.alpha,
+  augend.constant.alias.Alpha,
+}
+
+local cyclic_augends = {}
 for enum_name, string_list in pairs(addtional_enums) do
-  dial.augends["me#" .. enum_name] = dial.common.enum_cyclic{
-      name = enum_name,
-      strlist = string_list,
-  }
-  table.insert(dial.config.searchlist.normal, "me#" .. enum_name)
+  table.insert(cyclic_augends, augend.constant.new{
+    elements = string_list,
+    word = false,
+    cyclic = true,
+  })
 end
+
+local extra_augends = {
+  augend.hexcolor.new{
+    case = "lower",
+  },
+  augend.hexcolor.new{
+    case = "upper",
+  }
+}
+
+require("dial.config").augends:register_group{
+  default = vim.tbl_extend("keep", built_in_augends, extra_augends, cyclic_augends),
+}
