8fbb8c059d47f4d581a5713d6780210b7d84c6f9
Colin Shea <colin.shea@irl.com>
2022-04-14 12:14:39 -0700

    try improving the virtualenv experience with pylsp

--- config/nvim/lua/me/lsp/configs/pylsp.lua
+++ config/nvim/lua/me/lsp/configs/pylsp.lua
@@ -1,10 +1,17 @@
 -- I like pylint over pyflakes, so disable pyflakes and instead use pylint
 -- Also disable pycodestyle since I use black to format the code
 -- TODO: read from project-specific config instead of setting this globally
-return {
+local is_venv = vim.env.VIRTUAL_ENV ~= nil and vim.env.VIRTUAL_ENV ~= ''
+
+local python_path = vim.fn.exepath('python3')
+
+local lsp_settings = {
   settings = {
     pylsp = {
       plugins = {
+        jedi = {
+          environment = python_path
+        },
         pyflakes = {
           enabled = false
         },
@@ -15,10 +22,21 @@ return {
         pylint = {
           enabled = true
         },
+
+        pyls_mypy = {
+          enabled = true,
+          dmypy = true,
+          overrides = {"--python-executable", python_path, true}
+        }
       }
     }
   }
 }
 
+if is_venv then
+  -- lsp_settings[]
+end
+
+return lsp_settings
 -- Also note: my preferences require the installation of additional plugins to pylsp:
 -- run `:PylspInstall pylsp-mypy pyls-isort python-lsp-black pylsp-rope` in a neovim
