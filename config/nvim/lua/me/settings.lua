42cb6eab3576c1d752e32d2e5f470c7e112d390f
Colin Shea <colin@evaryont.me>
2022-05-01 22:15:02 -0700

    whoops, fix the condition

--- config/nvim/lua/me/settings.lua
+++ config/nvim/lua/me/settings.lua
@@ -28,7 +28,7 @@ if vim.fn.executable('neovim-ruby-host') then
   vim.g.ruby_host_prog = vim.fn.exepath('neovim-ruby-host')
 end
 -- use homebrew python if it exists (macOS support)
-if vim.fn.filereadable('/opt/homebrew/bin/python3') then
+if vim.fn.filereadable('/opt/homebrew/bin/python3') == 1 then
   vim.g.python3_host_prog = '/opt/homebrew/bin/python3'
 else
   -- otherwise presume the system-wide python3 is correct (which it is for Arch)
