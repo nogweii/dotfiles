eae34941d52ce0de760167f2d8b1b47654596057
Colin Shea <colin@evaryont.me>
2022-05-12 19:43:56 -0700

    make the lua-dev conditional load smarter
    now it encompasses all of the directories in 'packpath', not just my
    personal config folder!

--- config/nvim/lua/me/settings/lsp_servers/sumneko_lua.lua
+++ config/nvim/lua/me/settings/lsp_servers/sumneko_lua.lua
@@ -1,13 +1,25 @@
--- check if the Lua buffer opened just now is related neovim
--- (a config file underneath the correct directory)
-local neovim_parent_dir = vim.fn.resolve(vim.fn.stdpath("config"))
 local buffer_file_name = vim.fn.resolve(vim.fn.expand("%:p"))
 
-if string.sub(buffer_file_name, 1, string.len(neovim_parent_dir)) == neovim_parent_dir then
+local setup_options = {}
+
+-- check if the Lua buffer opened just now is related neovim
+-- (a config file underneath the correct directory)
+local directories = vim.split(vim.o.packpath, ",")
+local is_a_neovim_dir = false
+for _, dir in pairs(directories) do
+  local dir_absolute = vim.fn.resolve(dir)
+
+  if string.sub(buffer_file_name, 1, string.len(dir_absolute)) == dir_absolute then
+    is_a_neovim_dir = true
+    break
+  end
+end
+
+if is_a_neovim_dir then
   -- yup, so load the awesome settings from folke to teach the LSP about
   -- neovim's APIs
-  return require("lua-dev").setup({})
+  return require("lua-dev").setup(setup_options)
 else
   -- not a neovim config file, so no special settings
-  return {}
+  return setup_options
 end
