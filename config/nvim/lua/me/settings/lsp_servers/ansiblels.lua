faa40c292a0500c9c34c6d05918353b1626d535c
Nogweii <colin@evaryont.me>
2022-09-23 21:50:53 -0700

    Add options for ansiblels

---
+++ config/nvim/lua/me/settings/lsp_servers/ansiblels.lua
@@ -0,0 +1,13 @@
+local lsp_settings = {
+    settings = {
+        -- See https://als.readthedocs.io/en/latest/settings/ for a reference
+        ansible = {
+            completion = {
+                provideRedirectModules = false,
+                provideModuleOptionAliases = false
+            }
+        }
+    }
+}
+
+return lsp_settings
