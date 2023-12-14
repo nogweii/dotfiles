3aa2465b1746d5eb86fd68e24dd64a3973781ddf
Nogweii <me@nogweii.net>
2023-12-13 23:56:01 -0700

    crystal syntax plugin

--- config/nvim/lua/me/plugins/extra_file_types.lua
+++ config/nvim/lua/me/plugins/extra_file_types.lua
@@ -17,4 +17,16 @@ return {
 
   { "Joorem/vim-haproxy" },
 
+  {
+    "vim-crystal/vim-crystal",
+    init = function()
+      -- Disable the key mappings the plugin would make, most of them are redundant since I use
+      -- specific plugins to handle the (generic) functionality
+      vim.g.crystal_define_mappings = 0
+      -- I'll handle integrating the various tools with the other purpose-focused plugins
+      vim.g.crystal_auto_format = 0
+      -- cmp & the LSPs handle this for me, don't need the basic completion this plugin provides
+      vim.g.crystal_enable_completion = 0
+    end,
+  },
 }
