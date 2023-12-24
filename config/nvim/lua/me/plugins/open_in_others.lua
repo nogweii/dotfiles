a7a34a2d1816fc954c38264e18c46fb24be21812
Nogweii <me@nogweii.net>
2023-12-23 21:46:01 -0700

    dockerfile url patterns & stylua

--- config/nvim/lua/me/plugins/open_in_others.lua
+++ config/nvim/lua/me/plugins/open_in_others.lua
@@ -10,7 +10,7 @@ return {
     cmd = "URLOpenUnderCursor",
     event = "VeryLazy",
     keys = {
-      { "gou", ":URLOpenUnderCursor<cr>", mode = {"n"}, desc = "Open the URL under the cursor" },
+      { "gou", ":URLOpenUnderCursor<cr>", mode = { "n" }, desc = "Open the URL under the cursor" },
     },
     opts = {
       open_only_when_cursor_on_url = true,
@@ -18,31 +18,57 @@ return {
         all_urls = {
           enabled = false,
         },
-        cursor_move  = {
+        cursor_move = {
           enabled = true,
-        }
+        },
       },
       deep_pattern = true,
 
-      -- a list of patterns to open custom text as URLs
+      -- a list of patterns to open custom text as URLs.
+      -- inspiration from the official plugin https://github.com/sontungexpt/url-open/blob/main/lua/url-open/modules/patterns.lua
+      -- patterns are documented by lua at https://www.lua.org/pil/20.2.html
       extra_patterns = {
         {
-        	  pattern = '([\'"])([^/])/([^/])%1',
-        	  prefix = "https://github.com/",
-        	  suffix = "",
-        	  file_patterns = { "nvim/lua/.%S%.lua" },
-        	  excluded_file_patterns = nil,
+          pattern = "(['\"])([^/])/([^/])%1",
+          prefix = "https://github.com/",
+          suffix = "",
+          file_patterns = { "nvim/lua/.%S%.lua" },
+          excluded_file_patterns = nil,
         },
 
         {
-            pattern = 'gem [\'"]([^%s]*)[\'"]',
-        	  prefix = "https://rubygems.org/gems/",
-        	  suffix = "",
-        	  file_patterns = { "Gemfile", "gems.rb" },
-        	  excluded_file_patterns = nil,
+          pattern = "gem ['\"]([^%s]*)['\"]",
+          prefix = "https://rubygems.org/gems/",
+          suffix = "",
+          file_patterns = { "Gemfile", "gems.rb" },
+          excluded_file_patterns = nil,
         },
-      }
+
+        -- Dockerfile images: unprefix, unnamespaced (the library images, like `ruby:3.2`)
+        -- results in: https://hub.docker.com/_/ruby/
+        {
+          pattern = "^FROM ([^:.]+):",
+          prefix = "https://hub.docker.com/_/",
+          suffix = "/",
+          file_patterns = { "Dockerfile%S*", "Containerfile%S*" },
+          excluded_file_patterns = nil,
+          extra_condition = function(matched_pattern)
+            return not matched_pattern:match("/")
+          end,
+        },
+        -- Dockerfile images: unprefixed but namespaced (like crystallang/crystal)
+        -- results in: https://hub.docker.com/r/crystallang/crystal
+        {
+          pattern = "FROM ([^:.]+):",
+          prefix = "https://hub.docker.com/r/",
+          suffix = "/",
+          file_patterns = { "Dockerfile%S*", "Containerfile%S*" },
+          excluded_file_patterns = nil,
+          extra_condition = function(matched_pattern)
+            return matched_pattern:match("/")
+          end,
+        },
+      },
     },
   },
-
 }
