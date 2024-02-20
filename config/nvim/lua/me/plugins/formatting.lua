ba1d2fee3aecf139dc7f1511831fd3096a80238e
Nogweii <me@nogweii.net>
2024-02-20 01:15:58 -0700

    add text-case for changing case formats

--- config/nvim/lua/me/plugins/formatting.lua
+++ config/nvim/lua/me/plugins/formatting.lua
@@ -45,4 +45,17 @@ return {
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
+
+  {
+    'johmsalas/text-case.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('textcase').setup({})
+      require('telescope').load_extension('textcase')
+    end,
+    keys = {
+      '<leader>tc',
+      { '<leader>tc.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
+    },
+  },
 }
