2159dec404dc45cb1df26c6029b3710c822e9a20
Nogweii <me@nogweii.net>
2024-02-25 21:53:29 -0700

    svccurlog - get the logs of the latest run of a service
    and it's completion, though it's a bit janky sadly

---
+++ zsh/51_function_completions.zsh
@@ -0,0 +1,16 @@
+# I have a bunch of functions in ~/dotfiles/zsh/functions/ and let's make using them much nicer with
+# appropriate tab completion!
+
+function _systemd_units()
+{
+    # Load the various helper functions that the _systemctl compdef defines,
+    # if it hasn't been already
+    # TODO: this is a hack, the first time it runs it will return the regular systemctl completion
+    (( $+functions[_systemctl_all_units] )) || _systemctl
+    _systemctl_all_units
+
+    _wanted systemd-units expl unit \
+        compadd "$@" -a - _sys_all_units
+}
+
+compdef _systemd_units svccurlog
