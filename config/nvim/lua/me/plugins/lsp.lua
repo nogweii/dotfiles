a96a292af24923d396f300b6512156d32237d9fa
Colin <colin.shea@qwick.com>
2024-02-27 09:50:10 -0700

    fix inconsistent casing of SchemaStore.nvim

--- config/nvim/lua/me/plugins/lsp.lua
+++ config/nvim/lua/me/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
       -- see config/nvim/lua/me/settings/lsp_servers/lua_ls.lua for additional details
       'folke/neodev.nvim',
 
-      'b0o/schemastore.nvim',
+      'b0o/SchemaStore.nvim',
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
