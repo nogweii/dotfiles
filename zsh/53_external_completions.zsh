73ee380af0dae7127749fc27d474c2cf374cac6a
Colin <colin.shea@qwick.com>
2024-03-05 10:10:33 -0700

    new file to collect external package's installed zsh scripts

---
+++ zsh/53_external_completions.zsh
@@ -0,0 +1,25 @@
+# Additional shell completion functions from system packages
+
+#########################
+# fzf
+#########################
+
+# For Linux installs
+[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
+[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
+# For MacOS homebrew installs
+[ -f /opt/homebrew/opt/fzf/shell/completion.zsh ] && source /opt/homebrew/opt/fzf/shell/completion.zsh
+[ -f /opt/homebrew/opt/fzf/shell/key-bindings.zsh ] && source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
+
+#########################
+# Docker Desktop for Mac
+#########################
+
+if [ -d /Applications/Docker.app/Contents/Resources/etc ]; then
+  source /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion
+  source /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion
+
+  compdef _docker docker
+  compdef _docker dockerd
+  compdef _docker-compose docker-compose
+fi
