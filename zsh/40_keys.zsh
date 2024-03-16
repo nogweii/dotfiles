6d5e6036ef3a8b9a8d077a07c87e362acc6b993d
Nogweii <me@nogweii.net>
2024-03-15 23:30:32 -0700

    add autopairs to zsh

--- zsh/40_keys.zsh
+++ zsh/40_keys.zsh
@@ -69,3 +69,11 @@ bindkey -a ys add-surround
 
 source ${DOTSDIR}/zsh/plugins/vi-more-increment/vi-increment.zsh
 source ${DOTSDIR}/zsh/plugins/vi-more-quote/vi-quote.zsh
+
+source ${DOTSDIR}/zsh/plugins/autopair/autopair.zsh
+# Add an additional pair character:
+AUTOPAIR_PAIRS+=("<" ">")
+# To remove pairs, use:
+#unset 'AUTOPAIR_PAIRS[<]'
+# Then, do all of the key bindings:
+autopair-init
