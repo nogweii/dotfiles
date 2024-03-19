b4fde34b6b1628588acca0d4f56ba2a35362370f
Nogweii <me@nogweii.net>
2024-03-18 22:53:58 -0700

    change vcs background to a dark orange
    better than a nearly indistinguishable black IMO

--- zsh/55_prompt.zsh
+++ zsh/55_prompt.zsh
@@ -295,7 +295,7 @@ functions -M prompt_git_formatter
   # typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
   # typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
   # typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
-  typeset -g POWERLEVEL9K_VCS_BACKGROUND=0
+  typeset -g POWERLEVEL9K_VCS_BACKGROUND='#653208'
 
   # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
   typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
