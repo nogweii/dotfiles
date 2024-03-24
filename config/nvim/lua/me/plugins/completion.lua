dc3b0e39454a4a71a93907b45c000e9e20904f2b
Nogweii <me@nogweii.net>
2024-03-24 08:50:10 -0700

    move luasnip config into plugins/

--- config/nvim/lua/me/plugins/completion.lua
+++ config/nvim/lua/me/plugins/completion.lua
@@ -8,7 +8,20 @@ return {
       { 'honza/vim-snippets', version = false },
     },
     config = function()
-      require('me.settings.luasnip')
+      local luasnip = require('luasnip')
+
+      luasnip.config.set_config({
+        updateevents = 'TextChanged,TextChangedI',
+      })
+
+      -- One peculiarity of honza/vim-snippets is that the file containing global
+      -- snippets is _.snippets, so we need to tell luasnip that the filetype "_"
+      -- contains global snippets:
+      luasnip.filetype_extend('all', { '_' })
+
+      require('luasnip.loaders.from_vscode').lazy_load()
+      require('luasnip.loaders.from_snipmate').lazy_load()
+      require('luasnip.loaders.from_lua').lazy_load()
     end,
     version = 'v2.*',
   },
