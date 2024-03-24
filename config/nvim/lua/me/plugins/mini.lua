0252130ce571e1618ba46c3b81a25fb2426ddab8
Nogweii <me@nogweii.net>
2024-03-24 09:21:26 -0700

    set up mini.indentscope

--- config/nvim/lua/me/plugins/mini.lua
+++ config/nvim/lua/me/plugins/mini.lua
@@ -74,6 +74,30 @@ return {
             },
           },
         })
+
+        require('mini.indentscope').setup({
+          symbol = '│',
+          options = { try_as_border = true },
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+          pattern = {
+            'help',
+            'alpha',
+            'dashboard',
+            'neo-tree',
+            'Trouble',
+            'trouble',
+            'lazy',
+            'mason',
+            'notify',
+            'toggleterm',
+            'lazyterm',
+          },
+          callback = function()
+            ---@diagnostic disable-next-line: inject-field
+            vim.b.miniindentscope_disable = true
+          end,
+        })
       end,
       keys = {
         {
