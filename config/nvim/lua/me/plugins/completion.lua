a04d1c0635d2f91307677790bb9804719b997d29
Nogweii <me@nogweii.net>
2024-03-31 11:55:04 -0700

    disable the vim-snippets all-filetypes load
    a lot of these are *very* generic, and IMO, pollute the completion
    window. Disabling these as I don't think I'm getting any value from
    being able to put the current date or a copy of an OSS license.

--- config/nvim/lua/me/plugins/completion.lua
+++ config/nvim/lua/me/plugins/completion.lua
@@ -17,7 +17,7 @@ return {
       -- One peculiarity of honza/vim-snippets is that the file containing global
       -- snippets is _.snippets, so we need to tell luasnip that the filetype "_"
       -- contains global snippets:
-      luasnip.filetype_extend('all', { '_' })
+      -- luasnip.filetype_extend('all', { '_' })
 
       require('luasnip.loaders.from_vscode').lazy_load()
       require('luasnip.loaders.from_snipmate').lazy_load()
