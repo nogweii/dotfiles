aac35e1884d95789800e22f55fd8c62607ef55da
Colin <colin.shea@qwick.com>
2024-07-02 15:14:53 -0700

    a script to easily see a expanded yaml file

---
+++ bin/yaml-expanded.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'yaml'
+require 'json'
+
+if ARGV[0]
+  path = ARGV[0]
+else
+  warn 'You need to specify a file'
+  exit 1
+end
+
+original_yaml = YAML.load_file(path, aliases: true)
+json_vers = original_yaml.to_json
+expanded_yaml = YAML.load(json_vers)
+puts expanded_yaml.to_yaml
