f130919866d1c029d359ed3096ca05fd0d70c28c
Nogweii <me@nogweii.net>
2024-10-29 09:26:11 -0700

    try disabling css lsp formatting

--- etc/nvim/lua/me/settings/lsp_servers/cssls.lua
+++ etc/nvim/lua/me/settings/lsp_servers/cssls.lua
@@ -1,4 +1,22 @@
-local setup_options = {}
+local setup_options = {
+  settings = {
+    css = {
+      format = {
+        enable = false,
+      }
+    },
+    less = {
+      format = {
+        enable = false,
+      }
+    },
+    scss = {
+      format = {
+        enable = false,
+      }
+    }
+  }
+}
 
 if vim.fn.executable('vscode-css-languageserver') == 1 then
   -- the Arch Linux package installs the language server with a slightly
