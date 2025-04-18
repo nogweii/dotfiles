583514740df7e16157e03e0aef191cdee8ee3b1f
Nogweii <me@nogweii.net>
2025-04-17 19:29:09 -0700

    switch from versioned releases of plenary

--- etc/nvim/lua/me/plugins/libs.lua
+++ etc/nvim/lua/me/plugins/libs.lua
@@ -2,6 +2,6 @@
 return {
   { 'nvim-tree/nvim-web-devicons', lazy = true },
   { 'MunifTanjim/nui.nvim',        lazy = true },
-  { 'nvim-lua/plenary.nvim',       lazy = true },
+  { 'nvim-lua/plenary.nvim',       lazy = true, version = false },
   { 'tpope/vim-repeat' },
 }
